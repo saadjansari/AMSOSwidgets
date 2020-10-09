26d3b384858330f24b0ebe85fb8eb3e63b23e5b6
Adam Lamson <alamson@flatironinstitute.org>
2020-10-09 17:17:54 -0400

    Updated sim.py with pathlib conventions.

--- Analysis/sim.py
+++ Analysis/sim.py
@@ -8,27 +8,28 @@ import matplotlib.pyplot as plt
 import progressbar
 import pandas as pd
 
+from pathlib import Path
 from decorators import timer
-from Frame import *
+from Frame import Frame
 
 
 # Class for processing a complete sim
 class Sim():
-    def __init__(self, folder_name, label, opts):
+    def __init__(self, folder_path, label, opts):
 
         self.label = label
         self.opts = opts
-        self.folder_name = folder_name
+        self.folder_path = folder_path
         # path to a binary file storing useful processed data
-        self.feather_path = os.path.join(self.folder_name, 'df.ftr')
+        self.feather_path = self.folder_path / 'df.ftr'
 
     def read_config(self):
         # Read config file and store useful quantities
 
         # Load parameters from RunConfig.yaml and ProteinConfig.yaml files
-        with open(os.path.join(self.folder_name, 'RunConfig.yaml')) as f:
+        with open(self.folder_path / 'RunConfig.yaml') as f:
             pars1 = yaml.load(f, Loader=yaml.FullLoader)
-        with open(os.path.join(self.folder_name, 'ProteinConfig.yaml')) as f:
+        with open(self.folder_path / 'ProteinConfig.yaml') as f:
             pars2 = yaml.load(f, Loader=yaml.FullLoader)
 
         self.opts.dt = pars1['timeSnap']  # Specify frame step of simulation
@@ -37,10 +38,10 @@ class Sim():
 
         # Create plots directory
         if self.opts.graph:
-            rpath = os.path.join(self.folder_name, 'plots')
-            if os.path.exists(rpath):
+            rpath = self.folder_path / 'plots'
+            if rpath.exists():
                 shutil.rmtree(rpath)
-            os.mkdir(rpath)
+            rpath.mkdir()
 
     def analyze(self):
         # Analyze the sim
@@ -50,7 +51,7 @@ class Sim():
         print('Sim: {}'.format(self.label))
 
         # Load the processed binary file in case it exists
-        if not self.opts.overwrite and os.path.exists(self.feather_path):
+        if not self.opts.overwrite and self.feather_path.exists():
             print('\tLoading feather')
             self.df = pd.read_feather(self.feather_path)
             return
@@ -60,24 +61,20 @@ class Sim():
         self.df = pd.DataFrame()
         # Locate ascii data for filaments and proteins
         sfiles = sorted(
-            glob.glob(
-                os.path.join(
-                    self.folder_name,
-                    'result/result*/SylinderAscii_*.dat')),
+            [i for i in self.folder_path.glob(
+                'result/result*/SylinderAscii_*.dat')],
             key=fileKey)
         pfiles = sorted(
-            glob.glob(
-                os.path.join(
-                    self.folder_name,
-                    'result/result*/ProteinAscii_*.dat')),
+            [i for i in self.folder_path.glob(
+                'result/result*/ProteinAscii_*.dat')],
             key=fileKey)
 
         # Create progress bar
         bar = progress_bar(len(sfiles))
-        for idx in range(len(sfiles)):
+        for idx, (sf, pf) in enumerate(zip(sfiles, pfiles)):
 
             # Initialize frame instance and launch analysis
-            frame = Frame(sfiles[idx], pfiles[idx], self.opts)
+            frame = Frame(sf, pf, self.opts)
             frame.analyze()
             self.df = self.df.append(frame.data, ignore_index=True)
             bar.update(idx + 1)
@@ -101,10 +98,11 @@ def progress_bar(max_index):
 
 
 def fileKey(f):
+    fname = f.name
     # A function that returns the file index.
     # e.g. filekey('SylinderAscii_59.dat) -> 59
     # Get key to sort through file indices
-    k = int(f[f.rfind("_") + 1:f.rfind(".")])
+    k = int(fname[fname.rfind("_") + 1:fname.rfind(".")])
     return k
 
 
