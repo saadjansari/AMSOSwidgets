Fixed length calculation in Frame.py

Analysis/Frame.py
@@ -95,8 +95,13 @@ class Frame():
             ends_cc = np.vstack( (
                 np.array( df_sylinder.pos0.tolist())[cc,:], 
                 np.array( df_sylinder.pos1.tolist())[cc,:]))
-            self.data['tactoid_aspect_ratio'] = calc_aspect_ratio( 
-                    unfold_coordinates( ends_cc, self.opts.boxsize))
+            # unfold coordinates
+            if np.any( ends_cc):
+                ends_cc_unfolded = unfold_coordinates( 
+                        ends_cc, ends_cc[0,:], self.opts.boxsize)
+            else:
+                ends_cc_unfolded = ends_cc
+            self.data['tactoid_aspect_ratio'] = calc_aspect_ratio( ends_cc_unfolded)
 
         if self.opts.analyze_z_ordering:
             self.data['z_order'] = calc_z_ordering(
@@ -110,10 +115,11 @@ class Frame():
 
         # Length distribution inside vs outside cluster
         if self.opts.length_distribution:
-            len_fils = calc_distance_pbc(
+            dist_xyz = calc_distance_pbc(
                 np.array(df_sylinder.pos0.tolist()),
                 np.array(df_sylinder.pos1.tolist()),
                 self.opts.boxsize)
+            len_fils = np.sqrt( np.sum( len_fils**2, axis=1))
             self.data['length_mean_bulk'] = np.mean(len_fils)
             self.data['length_mean_cluster'] = np.mean(len_fils[cc_bool])
             self.data['length_mean_env'] = np.mean(